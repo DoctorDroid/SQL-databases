98e94e463de6972cc524482f960d3bf0a41517a5
Timothy Eakin <61610170+DoctorDroid@users.noreply.github.com>
2020-09-17 00:37:51 -0400

    First 6 of 8 complete. .py file included but not complete.

---
+++ module1-introduction-to-sql/DSPT7- INTRO2SQL.py
@@ -0,0 +1,45 @@
+import sqlite3
+import pandas as pd
+
+conn = sqlite3.connect('rpg_db.sqlite3')
+cursor = conn.cursor()
+
+def get_data(query, conn):
+    '''Function to get data from SQLite DB'''
+
+    cursor = conn.cursor()
+    result = cursor.execute(query).fetchall()
+
+    # Get columns from cursor object
+    columns = list(map(lambda x: x[0], cursor.description))
+
+    # Assign to DataFrame
+    df = pd.DataFrame(data=result, columns=columns)
+    return df
+
+q = get_data('SELECT COUNT(character_id) FROM charactercreator_character', conn)
+q0 = get_data('SELECT COUNT(item_id) FROM charactercreator_character_inventory', conn)
+q1 = get_data('SELECT COUNT(DISTINCT item_id) FROM charactercreator_character_inventory', conn)
+q2 = get_data('SELECT COUNT(character_ptr_id) FROM charactercreator_cleric;', conn)
+q3 = get_data('SELECT COUNT(character_ptr_id) FROM charactercreator_fighter;', conn)
+q4 = get_data('SELECT COUNT(character_ptr_id) FROM charactercreator_mage;', conn)
+q5 = get_data('SELECT COUNT(mage_ptr_id) FROM charactercreator_necromancer;', conn)
+q6 = get_data('SELECT COUNT(character_ptr_id) FROM charactercreator_thief;', conn)
+q7 = get_data('SELECT COUNT(character_ptr_id) FROM charactercreator_cleric;', conn)
+q8 = get_data('SELECT COUNT(character_ptr_id) FROM charactercreator_cleric;', conn)
+
+print('Number of Characters : ' + str(q['COUNT(character_id)'][0]))
+
+print('Number of Clerics : ' + str(q2['COUNT(character_ptr_id)'][0]))
+print('Number of Fighters : ' + str(q3['COUNT(character_ptr_id)'][0]))
+print('Number of Mages : ' + str(q4['COUNT(character_ptr_id)'][0]))
+print('Number of Necromancers : ' + str(q5['COUNT(mage_ptr_id)'][0]))
+print('Number of Thieves : ' + str(q6['COUNT(character_ptr_id)'][0]))
+
+print('Total Items: ' + str(q0['COUNT(item_id)'][0]))
+print('Distinct Items: ' + str(q1['COUNT(DISTINCT item_id)'][0]))
+
+print('Question 7 : ' + str(q7['COUNT(character_ptr_id)'][0]))
+print('Question 8 : ' + str(q8['COUNT(character_ptr_id)'][0]))
+
+
